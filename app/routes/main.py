4eb4460b262e535ba99388bb9c59d8bd0c37f626
IceFox27 <erofey.karpov@gmail.com>
2026-04-22 13:52:24 +1000

    Добавлены прототипы страниц для гостевой

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,3 +5,27 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('main/index.html')
+
+@main.route('/about')
+def about():
+    return render_template('main/about.html')
+
+@main.route('/portfolio')
+def portfolio():
+    return render_template('main/portfolio.html')
+
+@main.route('/services')
+def services():
+    return render_template('main/services.html')
+
+@main.route('/jobs')
+def jobs():
+    return render_template('main/jobs.html')
+
+@main.route('/contacts')
+def contacts():
+    return render_template('main/contacts.html')
+
+@main.route('/blog')
+def blog():
+    return render_template('main/blog.html')
